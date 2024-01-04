9e08f7946e275174bb40b484357a47c1f79fb94b
Jack Heaton <75635917+EpictetusZ1@users.noreply.github.com>
2024-01-04 16:08:42 -0500

    very basic game construction

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,8 @@ pub fn read_file() -> Result<(), Box<dyn Error>> {
     if let Some(last_move) = moves.last() {
         let parts: Vec<&str> = last_move.split_whitespace().collect();
         if let Some(result) = parts.last() {
-            println!("The result is: {result}");
+            let game = build_game(tags, &moves, parse_result(result));
+            println!("DATA BUILT: {:?}", game);
         } else {
             println!("Could not find the result in the last move."); // maybe handle this case with U?
         }
@@ -26,18 +27,6 @@ pub fn read_file() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-pub enum GameResult {
-    W,
-    B,
-    D,
-}
-
-pub struct Game {
-    tags: HashMap<String, String>,
-    moves: HashMap<usize, String>,
-    result: GameResult
 }
 
 pub fn split_game_data(contents: &str) -> (Vec<&str>, Vec<&str>) {
@@ -63,6 +52,57 @@ pub fn split_game_data(contents: &str) -> (Vec<&str>, Vec<&str>) {
     }
 
     (tags, moves)
+}
+
+#[derive(Debug)]
+pub enum GameResult {
+    W,
+    B,
+    D,
+}
+
+#[derive(Debug)]
+pub struct Game {
+    tags: HashMap<String, String>,
+    moves: HashMap<usize, String>,
+    result: GameResult
+}
+
+pub fn split_tags(tag: &str) -> (String, String) {
+    let parts: Vec<&str> = tag.split_whitespace().collect();
+    if parts.len() >= 2 {
+        (parts[0].to_string(), parts[1].to_string())
+    } else {
+        panic!("Tag is not in the expected format: {}", tag)
+    }}
+
+pub fn build_game(tags: Vec<&str>, move_list: &Vec<&str>, g_result: GameResult) -> Game  {
+    let mut format_tags: HashMap<String, String> = HashMap::new();
+    let mut format_moves: HashMap<usize, String> = HashMap::new();
+
+    for tag in tags {
+        let (key, value) = split_tags(tag);
+        format_tags.insert(key, value);
+    }
+
+    for (index, m) in move_list.iter().enumerate() {
+        format_moves.insert(index + 1, m.to_string());
+    }
+
+    Game {
+        tags: format_tags,
+        moves: format_moves,
+        result: g_result,
+    }
+}
+
+pub fn parse_result(result_str: &str) -> GameResult {
+    match result_str {
+        "1-0" => GameResult::W,
+        "0-1" => GameResult::B,
+        "0.5-0.5" | "1/2-1/2" => GameResult::D,
+        _ => panic!("Unexpected game result string: {}", result_str),
+    }
 }
 
 #[cfg(test)]
